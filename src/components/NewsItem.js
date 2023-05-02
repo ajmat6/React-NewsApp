72d6baf0501ab27f4b495cf49a5a9c46982571fb
ajmat6 <114720514+ajmat6@users.noreply.github.com>
2023-05-02 21:03:40 +0530

    Using Fetch API for http calls + componentDidMount life cycle + setting slice property in title and description

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,8 +7,8 @@ export class NewsItem extends Component {
     let {title,description, imageurl, newsurl} = this.props;  
     return (
       <div className='my-3'>
-        <div className="card" style={{width: '18rem'}}>
-            <img src={imageurl} className="card-img-top" alt="..." />
+        <div className="card" style={{width: '18rem', height: '25rem'}}>
+            <img src={imageurl == null?imageurl='https://www.brandingmag.com/wp-content/uploads/2023/04/COVER_001_AndiDavids_1200x676-1200x628.jpg':imageurl} className="card-img-top" alt="..." />
             <div className="card-body">
                 <h5 className="card-title">{title}...</h5>
                 <p className="card-text">{description}...</p>
